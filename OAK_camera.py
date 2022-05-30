05f373b54a3b04c901ddbf21949d12d17791a208
pbelhe <prasad@digitalbasis.com>
2022-05-31 09:32:41 +1000

    Threaded Camera Controller

--- OAK_camera.py
+++ OAK_camera.py
@@ -1,116 +1,127 @@
-from asyncio.windows_events import NULL
-from re import T
-import sys 
-from PyQt5.uic import loadUi
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QDialog, QMainWindow, QComboBox
-from matplotlib import widgets
-from matplotlib.widgets import Widget
-from PyQt5.QtGui import QPixmap
+from cProfile import run
+import sys
 import cv2
 #from cv2 import VideoCapture
 import depthai as dai
 import time
 import os
+import threading
+import time
 
-# Camera Setup
-pipeline = dai.Pipeline()
+class OAK_camera(threading.Thread):
+    ip = "192.168.1.100"
+    display = None
+    killed = False
+    pipeline = dai.Pipeline()
 
-camRgb = pipeline.createColorCamera()
+    camRgb = pipeline.createColorCamera()
+    manip = pipeline.createImageManip()
+    xoutRgb = pipeline.createXLinkOut()
 
-xoutRgb = pipeline.createXLinkOut()
-xoutRgb.setStreamName("rgb")
-camRgb.preview.link(xoutRgb.input)
+    # Properties camRgb
+    camRgb.setPreviewSize(960, 512)
+    camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+    camRgb.setInterleaved(False)
+    camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+    camRgb.setFps(40)
 
-#Dashboard screen
-class Dashboard(QMainWindow):
-    cameraAction = None
-    def __init__(self):
-        super(Dashboard, self).__init__()
-        loadUi("gui.ui", self)
-        self.setStatus("Initiating, please wait")
-        time.sleep(3)
-        self.setStatus("Initiated")
-        self.test_btn.clicked.connect(self.testAction)
-        self.record_btn.clicked.connect(self.recordAction)
-        self.detect_btn.clicked.connect(self.detectAction)
+    # Properties manip
+    manip.setMaxOutputFrameSize(1474560)
+    rgbRr = dai.RotatedRect()
+    rgbRr.center.x, rgbRr.center.y = camRgb.getPreviewWidth() // 2, camRgb.getPreviewHeight() // 2
+    rgbRr.size.width, rgbRr.size.height = camRgb.getPreviewHeight(), camRgb.getPreviewWidth()
+    rgbRr.angle = -90
+    manip.initialConfig.setCropRotatedRect(rgbRr, False)
 
-    # Click through buttons functions
-    def testAction(self):
-        self.setStatus("Initiating Testing...")
-        if self.isRunning() is not None:
-            self.setStatus("Camera is " + self.isRunning())  # testing/recording/detecting
-        else:
-            self.isRunning("Testing")                       # lock for testing
-            self.setStatus("Initiating Testing")
-            time.sleep(3)
-            os.system("start /wait cmd /c python OAK_test_setup.py")
-            time.sleep(3)
-            self.isRunning(None) 
-            self.setStatus("Testing Stopped")
+    # Properties xoutRgb
+    xoutRgb.setStreamName("preview")
 
-    def recordAction(self):
-        self.setStatus("Initiating Recording...")
-        if self.isRunning() is not None:
-            self.setStatus("Camera is " + self.isRunning())  # testing/recording/detecting
-        else:
-            # lock for recording
-            self.isRunning("Recording")
-            self.setStatus("Initiating Recording")
-            time.sleep(3)
-            self.setStatus("Initiating Recording..")
-            os.system("start /wait cmd /c python OAK_camera_to_video.py")
-            time.sleep(3)
-            self.isRunning(None) 
-            self.setStatus("Recording Stopped")
+    # Linking
+    camRgb.preview.link(manip.inputImage)
+    manip.out.link(xoutRgb.input)
+
+    #generate filename
+    dirname = "video"
+    dt_now = time.strftime("%Y%m%d-%H%M%S")
+    filepath =  os.path.join(os.path.dirname(os.path.abspath(__file__)), dirname,dt_now+".mp4")
+    out = None
+
+    device_info = None
+    # overriding constructor
+    def __init__(self, device_info = None, display = None):
+        # calling parent class constructor
+        threading.Thread.__init__(self)
+        self._stop_event = threading.Event()
+        self.device_info = device_info
+        self.ip = device_info.getMxId()
+        self.display = display
+        self.killed = False
+        self.camera_config()
+        self.generate_filename()
+
+    def camera_config(self):
         
+        self.pipeline = dai.Pipeline()
+        self.camRgb = self.pipeline.createColorCamera()
+        self.manip = self.pipeline.createImageManip()
+        self.xoutRgb = self.pipeline.createXLinkOut()
 
-    def detectAction(self):
-        self.setStatus("Initiating Detection...")
-        if self.isRunning() is not None:
-            self.setStatus("Camera is " + self.isRunning())  # testing/recording/detecting
-        else:
-            # lock for detection
-            self.isRunning("Detecting")
-            self.setStatus("Initiating Detection")
-            time.sleep(3)
-            text = self.filename_text.text()
-            if (len(text) == 19 and text.endswith(".mp4")):
-                filename = text.replace(".mp4","")
-                self.setStatus("Detecting "+text)
-                cmd = "ffmpeg -framerate 30 -i "+filename+".h265 -c copy "+filename+".mp4"
-                os.chdir("video")
-                os.system("start /wait cmd /c "+cmd)
-                os.chdir("..")
-                self.setStatus("Processed "+text)
-            else:
-                self.setStatus("Please enter valid filename..")
-            self.isRunning(None) 
+        # Properties camRgb
+        self.camRgb.setPreviewSize(960, 512)
+        self.camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+        self.camRgb.setInterleaved(False)
+        self.camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+        self.camRgb.setFps(40)
 
-    # Helper functions
-    def isRunning(self,status = NULL):
-        if status is not NULL:
-            self.cameraAction = status
-        if self.cameraAction is not None:
-            self.label_7.setPixmap(QtGui.QPixmap("hmi-images/online.png"))
-        else:
-            self.label_7.setPixmap(QtGui.QPixmap("hmi-images/offline.png"))
-        return self.cameraAction
+        # Properties manip
+        self.manip.setMaxOutputFrameSize(1474560)
+        rgbRr = dai.RotatedRect()
+        rgbRr.center.x, rgbRr.center.y = self.camRgb.getPreviewWidth() // 2, self.camRgb.getPreviewHeight() // 2
+        rgbRr.size.width, rgbRr.size.height = self.camRgb.getPreviewHeight(), self.camRgb.getPreviewWidth()
+        rgbRr.angle = -90
+        self.manip.initialConfig.setCropRotatedRect(rgbRr, False)
 
-    def setStatus(self,status):
-        self.label_2.setText(str(status)) 
-    # End click through buttons functions
+        # Properties xoutRgb
+        self.xoutRgb.setStreamName("preview")
 
-# Main 
-app = QApplication(sys.argv)
-widget = QtWidgets.QStackedWidget()
-dashboard = Dashboard()
-widget.addWidget(dashboard)
-widget.setFixedHeight(768)
-widget.setFixedWidth(1366)
-widget.show()
+        # Linking
+        self.camRgb.preview.link(self.manip.inputImage)
+        self.manip.out.link(self.xoutRgb.input)
+        print("Camera Configured "+self.device_info.getMxId())
 
-try: 
-    sys.exit(app.exec_())
-except:
-    print("Exiting")
+    def generate_filename(self):
+        #generate filename
+        if not os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), self.dirname)):
+            os.makedirs(os.path.join(os.path.dirname(os.path.abspath(__file__)), self.dirname))
+        self.dt_now = time.strftime("%Y%m%d-%H%M%S")+"_"+self.ip.replace(".","_")
+        self.filepath =  os.path.join(os.path.dirname(os.path.abspath(__file__)), self.dirname,self.dt_now+".mp4")
+
+        # delete outpy.mp4 if exist
+        print("File: "+self.dt_now)
+        if os.path.exists(self.filepath):
+            os.remove(self.filepath)
+        self.out = cv2.VideoWriter(self.filepath, cv2.VideoWriter_fourcc('m','p','4','v'), 40, (512,960))
+        print("Video will be Saved in "+self.filepath)
+
+    # define your own run method
+    def run(self):
+        time.sleep(3)
+        with dai.Device(self.pipeline, self.device_info) as device:
+            print("Recodring started, press q on frame to stop recording")
+            qRgb = device.getOutputQueue('preview', maxSize=1, blocking=False)
+            while True:
+                frame = qRgb.get().getCvFrame()
+                self.out.write(frame)
+                cv2.imshow(self.ip, frame )
+                if cv2.waitKey(1) == ord('q'):
+                    break
+        self.out.release()
+        print("File Saved successfully: "+self.dt_now+".mp4")
+        cv2.destroyWindow(self.ip)
+        self.stop()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
